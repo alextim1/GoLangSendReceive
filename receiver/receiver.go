29963e28525d270794b6c49bedade7d5ef8a2964
alextim1 <alexey.timohin@gmail.com>
2020-07-16 21:44:53 +0300

    log file creating added

--- receiver/receiver.go
+++ receiver/receiver.go
@@ -1,10 +1,14 @@
 // receiver works as an Server side
+// TODO!!!!! : Find an elegant solution (unlike uglie examples from Google)
+//to use CONTEXT for automatic
+//closing App receiver after getting -1 Flag for finishing broadcasting
 package main
 
 import (
 	"context"
 	"fmt"
 	"net"
+	"os"
 	"time"
 
 	tr "transport"
@@ -16,6 +20,7 @@ import (
 const (
 	PORT          = "9090"
 	LOGSPERSECOND = 2
+	LOGFILE       = "log.txt"
 )
 
 // Pipeline : Receive -> Select Go to Logger or to the TRASH box
@@ -38,9 +43,19 @@ func main() {
 
 	// Logging messages
 	go func(log chan tr.CarrierDto) {
+		path, _ := os.Getwd()
+		file, err := os.Create(path + "/" + LOGFILE)
+
+		if err != nil {
+			fmt.Println("Unable to create file:", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
 		msg := <-log
 
 		fmt.Println("logged: %i", msg.FibNumber)
+		file.WriteString(fmt.Sprintf("%d\n", msg.FibNumber))
 
 		for msg.FibNumber != -1 {
 			time.Sleep(time.Second / LOGSPERSECOND)
@@ -48,12 +63,14 @@ func main() {
 			msg = <-log
 
 			fmt.Println("logged: %i", msg.FibNumber)
+			file.WriteString(fmt.Sprintf("%d\n", msg.FibNumber))
 		}
 
 		fmt.Println("exit logger")
 
 	}(log)
 
+	// Throw to the trash box
 	go func(trash chan tr.CarrierDto) {
 		msg := <-trash
 
